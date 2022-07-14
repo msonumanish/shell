59035efe3db74ff4bb22cf70db8b91e1087557b0
Manish jain <msonu.manish@gmail.com>
2022-07-15 05:19:34 +0530

    log

--- logdir.sh
+++ logdir.sh
@@ -1,33 +1,10 @@
-#!/bin/bash -l
-
 export LOGDIR=/Users/mj/Documents/Udemy/docker/shellScript
 export DATE=`date +"%Y%m%d"`
 export DATETIME=`date +"%Y%m%d_%H%M%S"`
-NO_JOB_LOGGING="false"
 
 ScriptName=`basename $0`
-# Job=`basename $0 .sh`"_awhatever_I_want" # Add _whatever_I_want after basename
-Job=`basename $0 .sh`
+Job=`basename $0 .sh`"_whatever_I_want"
 JobClass=`basename $0 .sh`
-
-colblk='\033[0;30m' # Black - Regular
-colred='\033[0;31m' # Red
-colgrn='\033[0;32m' # Green
-colylw='\033[0;33m' # Yellow
-colpur='\033[0;35m' # Purple
-colwht='\033[0;97m' # White
-colrst='\033[0m'    # Text Reset
-
-verbosity=4
-
-### verbosity levels
-silent_lvl=0
-crt_lvl=1
-err_lvl=2
-wrn_lvl=3
-ntf_lvl=4
-inf_lvl=5
-dbg_lvl=6
 
 ## esilent prints output even in silent mode
 function esilent () { verb_lvl=$silent_lvl elog "$@" ;}
@@ -38,9 +15,7 @@ function einfo ()  { verb_lvl=$inf_lvl elog "${colwht}INFO${colrst} ---- $@" ;}
 function edebug () { verb_lvl=$dbg_lvl elog "${colgrn}DEBUG${colrst} --- $@" ;}
 function eerror () { verb_lvl=$err_lvl elog "${colred}ERROR${colrst} --- $@" ;}
 function ecrit ()  { verb_lvl=$crt_lvl elog "${colpur}FATAL${colrst} --- $@" ;}
-
 function edumpvar () { for var in $@ ; do edebug "$var=${!var}" ; done }
-
 function elog() {
         if [ $verbosity -ge $verb_lvl ]; then
                 datestring=`date +"%Y-%m-%d %H:%M:%S"`
@@ -50,10 +25,9 @@ function elog() {
 
 function Log_Open() {
         if [ $NO_JOB_LOGGING ] ; then
-                einfo "Not logging to a logfile because -Z option specified." #(*)
+                elog "Not logging to a logfile because -Z option specified." #(*)
         else
                 [[ -d $LOGDIR/$JobClass ]] || mkdir -p $LOGDIR/$JobClass
-                echo "${LOGDIR}/$JobClass/${Job}_${DATETIME}"
                 Pipe=${LOGDIR}/$JobClass/${Job}_${DATETIME}.pipe
                 mkfifo -m 700 $Pipe
                 LOGFILE=${LOGDIR}/$JobClass/${Job}_${DATETIME}.log
@@ -83,33 +57,15 @@ function Log_Close() {
         fi
 }
 
-
 OPTIND=1
-while getopts ":sVGZ" opt ; do
-# shellcheck disable=SC2220
-        case $opt in
-        s)
-                verbosity=$silent_lvl
-                edebug "-s specified: Silent mode"
-                ;;
-        V)
-                verbosity=$inf_lvl
-                edebug "-V specified: Verbose mode"
-                ;;
-        G)
-                verbosity=$dbg_lvl
-                edebug "-G specified: Debug mode"
-                ;;
-        Z)
-                NO_JOB_LOGGING="true"
-                ;;
+while getopts ":Z" opt ; do
+ case $opt in
+                Z)
+                  NO_JOB_LOGGING="true"
+                        ;;
         esac
 done
 
-# Options for logging
-ewarn "this is a warning"
-eerror "this is an error"
-einfo "this is an information"
-edebug "debugging"
-ecrit "CRITICAL MESSAGE!"
-edumpvar HOSTNAME
+Log_Open
+echo "whatever I execute here will be logged to $LOGFILE"
+Log_Close
